84d44fe2cb84c2d9f30c8823914ec85a48349c33
gokul <gokulakams@gmail.com>
2022-01-24 14:10:29 +0530

    partial completion of html compilation

---
+++ compile.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+HTML_SETTINGS="$PWD/.assets/html-plain,fn-in,charset=utf-8"
+CURRENT_DIR="$PWD"
+files=$(find -name '*.tex')
+mkdir -p docs/tmp
+cd docs/tmp
+for file in $files; do
+    output_dir=$(dirname "$file")
+    mkdir -p $CURRENT_DIR/docs/$output_dir
+    echo $output_dir
+    if [[ "$file" == *.tex ]]       
+    then
+        echo $file
+        make4ht $CURRENT_DIR/$file -d $CURRENT_DIR/docs/$output_dir
+    fi
+done
+cd $CURRENT_DIR
+rm -rf $CURRENT_DIR/docs/tmp
+tree -H './build' -L 1 --noreport --charset utf-8 -o docs/index.html
+
